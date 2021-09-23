e893978b81def00a926e5ed2e3da98c17e5def1d
Milena <milenarosa18@gmail.com>
2021-09-23 12:59:12 -0300

    jQuery

--- jQuery/alura-typer/public/js/main.js
+++ jQuery/alura-typer/public/js/main.js
@@ -1,28 +1,119 @@
-var frase = $(".frase").text();
-var numPalavras = frase.split(" ").length;
-
-var tamanhoFrase = $("#tamanho-frase");
-tamanhoFrase.text(numPalavras);
-
+var tempoInicial = $("tempo-digitacao").text();
 var campo = $(".campo-digitacao");
-campo.on("input",function(){
-   var conteudo = campo.val();
 
-   var qtPalavras = conteudo.split(/\S+/).length-1;
-   $("#contador-palavras").text(qtPalavras);
+$(document).ready(function(){
+    atualizaTamanhoFrase();
+    iniciaContadores();
+    inicializaCronometro();
+    $("#botao-reiniciar").click(reiniciaJogo);
 
-   var qtCaracteres = conteudo.length;
-   $("#contador-caracteres").text(qtCaracteres);
+}); //atalho $(fuction(){...});
+
+
+
+function atualizaTamanhoFrase(){
+    var frase = $(".frase").text();
+    var numPalavras = frase.split(" ").length;
+    var tamanhoFrase = $("#tamanho-frase");
+    tamanhoFrase.text(numPalavras);
+}
+
+function iniciaContadores (){
+    campo.on("input",function(){
+    var conteudo = campo.val();
+
+    var qtPalavras = conteudo.split(" ").length;
+    $("#contador-palavras").text(qtPalavras);
+
+    var qtCaracteres = conteudo.length;
+    $("#contador-caracteres").text(qtCaracteres);
 });
+}
 
-var tempoRestante = $("#tempo-digitacao").text();
-campo.one("focus",function(){
-    var cronometroID = setInterval(function(){
-        tempoRestante--;
-        $("#tempo-digitacao").text(tempoRestante);
-        if (tempoRestante < 1) {
-            campo.attr("disabled", true);
-            clearInterval(cronometroID);
+function inicializaCronometro (){
+    var tempoRestante = $("#tempo-digitacao").text();
+    campo.one("focus",function(){
+        var cronometroID = setInterval(function(){
+            tempoRestante--;
+            $("#tempo-digitacao").text(tempoRestante);
+            if (tempoRestante < 1) {
+                clearInterval(cronometroID);
+                finalizaJogo();
+            }
+        },1000);
+    });
+}
+
+function finalizaJogo(){
+    campo.attr("disabled", true);
+    campo.toggledClass("campo-desativado");
+    inserirPlacar();
+}
+
+function inicializaMarcadores() {
+    var frase = $(".frase").text();
+    campo.on("input",function(){
+        var digitado = campo.val();
+        var comparavel = frase.substr(0,digitado.length);
+        if(digitado == comparavel) {
+            campo.addClass("borda-verde");
+            campo.removeClass("borda-vermelha");
+        } else {
+            campo.addClass("borda-vermelha");
+            campo.removeClass("borda-verde");
         }
-    },1000);
-});
+    })
+}
+
+function inserirPlacar(){
+    var corpoTabela = $(".placar").find("tbody");
+    var usuario = "Seu-nome";
+    var numPalavras = $("#contador-palavras").text();
+    
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removerLinha);
+
+    corpoTabela.prepend(linha);
+
+}
+
+function novaLinha(usuario,palavras){
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(palavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").attr("href","#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    // Icone dentro do <a>
+    link.append(icone);
+    // <a> dentro do <td>
+    colunaRemover.append(link);
+
+    // Os três <td> dentro do <tr>
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+
+    return linha;
+}
+
+function removerLinha() {
+    Event.preventDefault();
+    $(this).parent().parent().remove();
+}
+
+
+function reiniciaJogo(){
+    campo.attr("disabled", false);
+    campo.val(" ");
+    $("#contador-palavras").text("0");
+    $("#contador-caracteres").text("0");
+    $(".tempo-digitacao").text(tempoInicial);
+    inicializaCronometro();
+    campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-verde");
+}
+
